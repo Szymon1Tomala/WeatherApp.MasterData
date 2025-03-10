48a9f97c72f1c3cbf18780ee1434be6d10172756
Szymon <szymon.tomala2@gmail.com>
2025-03-10 22:36:51 +0100

    properties added to user

--- src/Domain/Persistence/Configurations/UserConfiguration.cs
+++ src/Domain/Persistence/Configurations/UserConfiguration.cs
@@ -9,6 +9,12 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.ToTable("User");
+        
         builder.HasKey(x => x.Id);
+        
+        builder.Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.LastName).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.Email).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.Password).IsRequired().HasMaxLength(50);
     }
 }
